0011058cb982c2a67f873e869c2a8f7452c564dd
Arion <arion.strakosas@gmail.com>
2022-11-04 22:08:55 +0200

    new

--- alembic/env.py
+++ alembic/env.py
@@ -13,7 +13,7 @@ import psycopg2
 config = context.config
 config.set_main_option(
     "sqlalchemy.url",
-    "postgresql+psycopg2-binary://postgres:Tir1061985@localhost:5432/fastapi",
+    "postgresql+psycopg2://postgres:Tir1061985@localhost:5432/fastapi",
 )
 
 # Interpret the config file for Python logging.
